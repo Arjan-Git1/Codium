b88b07ad0f7e3f559c844048c81c944c112cfb30
Arjan Chakrabory <aryaarjan1@gmail.com>
2026-05-05 19:22:02 +0530

    done some shit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::{
+    event::{self, Event, KeyCode, KeyEvent, read},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
+fn main() -> io::Result<()> {
+    enable_raw_mode().expect("Error");
+    loop {
+        if let Event::Key(event) = event::read()? {
+            match event.code {
+                KeyCode::Char('q') => {
+                    break;
+                }
+                KeyCode::Char(c) => {
+                    println!("{}", c);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    disable_raw_mode().expect("Error");
+    Ok(())
 }
