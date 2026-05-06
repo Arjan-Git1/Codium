11494623f3fe8e304ef3632c25004669bcfcc981
Arjan Chakrabory <aryaarjan1@gmail.com>
2026-05-06 16:35:26 +0530

    igbro

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
-use std::io;
+use std::{
+    fmt::write,
+    io::{self, Write, stdout},
+};
 
 use crossterm::{
-    event::{self, Event, KeyCode, KeyEvent, read},
-    terminal::{disable_raw_mode, enable_raw_mode},
+    cursor::{self, MoveToNextLine},
+    event::{self, Event, KeyCode, KeyEventKind, KeyModifiers},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
 
 fn main() -> io::Result<()> {
-    enable_raw_mode().expect("Error");
+    enable_raw_mode()?;
+    execute!(io::stdout(), EnterAlternateScreen)?;
+    let mut buffer = String::new();
     loop {
         if let Event::Key(event) = event::read()? {
-            match event.code {
-                KeyCode::Char('q') => {
-                    break;
+            if event.kind == KeyEventKind::Press {
+                match (event.modifiers, event.code) {
+                    (KeyModifiers::CONTROL, KeyCode::Char('q')) => {
+                        break;
+                    }
+
+                    (_, KeyCode::Char(c)) => {
+                        buffer.push(c);
+                        write!(io::stdout(), "{}", buffer)?;
+                        stdout().flush()?;
+                        buffer.clear();
+                    }
+                    (_, KeyCode::Enter) => {
+                        println!();
+                        execute!(stdout(), MoveToNextLine(2))?;
+                    }
+                    //next day i have to add backspace and make code modules as the code is bhery
+                    //messsi iamsocoolmaneveryonelikesmeiamtheworstcoderofalltime
+                    _ => {}
                 }
-                KeyCode::Char(c) => {
-                    println!("{}", c);
-                }
-                _ => {}
             }
         }
     }
-
-    disable_raw_mode().expect("Error");
+    execute!(io::stdout(), LeaveAlternateScreen)?;
+    disable_raw_mode()?;
     Ok(())
 }
